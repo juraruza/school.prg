53f855003912c2f8795fb064e51921ee629e62df
juraruza <jirkacz118@seznam.cz>
2023-11-22 18:11:23 +0100

    procvičování

---
+++ prg07.py
@@ -0,0 +1,2 @@
+jmeno = input("Zadej své jméno: ")
+print(f"Ahoj, {jmeno}! Vítej ve světě programování.")
---
+++ prg08.py
@@ -0,0 +1,16 @@
+import random
+
+
+cislo_ke_hodnuti = random.randint(1, 10)
+
+pokusy = 0
+
+while True:
+    hadane_cislo = int(input("Uhodni číslo mezi 1 a 10: "))
+    pokusy += 1
+
+    if hadane_cislo == cislo_ke_hodnuti:
+        print(f"Skvělé! Uhodl(a) jsi číslo {cislo_ke_hodnuti} během {pokusy} pokusů.")
+        break
+    else:
+        print("Zkus to znovu. Špatné číslo.")
